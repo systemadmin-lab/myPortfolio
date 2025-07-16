d4151f7d74178c340389fc4b7d92ec016ef6a57c
Rejuan Ahamed Anik <rejuananik@gmail.com>
2025-07-17 02:27:37 +0600

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
         <p>|</p>
         <p>Privacy Policy</p>
       </div>
-      <div className="flex gap-3">
+{/*       <div className="flex gap-3">
         {mySocials.map((social, index) => (
           <a href={social.href} key={index}>
             <img src={social.icon} className="w-5 h-5" alt={social.name} />
           </a>
         ))}
-      </div>
+      </div> */}
       <p>© 2025 Rejuan. All rights reserved.</p>
     </section>
   );
